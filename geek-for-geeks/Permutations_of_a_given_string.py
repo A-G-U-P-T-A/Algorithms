c9d8fac23bfce4ba23ada3b9da0c5d295af225bf
ARNAB GUPTA <arnabgupta91996@gmail.com>
2018-09-26 16:52:47 +0530

    Create Permutations_of_a_given_string.py

---
+++ geek-for-geeks/Permutations_of_a_given_string.py
@@ -0,0 +1,28 @@
+"""
+Given a string, print all permutations of a given string.
+
+Input:
+The first line of input contains an integer T, denoting the number of test cases.
+Each test case contains a single string S in capital letter.
+
+Output:
+For each test case, print all permutations of a given string S with single space and all permutations should be in lexicographically increasing order.
+
+Constraints:
+1 ≤ T ≤ 10
+1 ≤ size of string ≤ 5
+
+Example:
+Input:
+2
+ABC
+ABSG
+Output:
+ABC ACB BAC BCA CAB CBA 
+ABGS ABSG AGBS AGSB ASBG ASGB BAGS BASG BGAS BGSA BSAG BSGA GABS GASB GBAS GBSA GSAB GSBA SABG SAGB SBAG SBGA SGAB SGBA
+"""
+l = [sorted(list(pt(input()))) for i in range(int(input()))]
+for i in l:
+    for j in i:
+        print(''.join(j),end=' ')
+    print()
